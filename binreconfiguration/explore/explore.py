e026abd99f5d5d632187b043bf13c5cb4236f6eb
Stéphane Vialette <vialette@gmail.com>
2015-03-06 23:17:45 +0100

    gradient descent

--- binreconfiguration/explore/explore.py
+++ binreconfiguration/explore/explore.py
@@ -4,6 +4,10 @@ class Explore(metaclass = abc.ABCMeta):
 
 	def __init__(self, storage_unit):
 		self._storage_unit = storage_unit
+		self._number_of_steps = 0
 
 	def storage_unit(self):
 		return self._storage_unit
+
+	def number_of_steps(self):
+		return self._number_of_steps
